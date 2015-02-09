b0492f499b7f33dc448635cb3bbe3154a359615b
J. Derek Tucker <jdtuck@sandia.gov>
2015-02-09 07:58:49 -0700

    fixed Makefile

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,5 @@
 @unix_only begin
     cd(joinpath(dirname(@__FILE__), "src", "fdasrsf"))
     suffix = @osx? "dylib" : "so"
-    run(`make CC=icc SUFFIX=$suffix`)
+    run(`make SUFFIX=$suffix`)
 end
