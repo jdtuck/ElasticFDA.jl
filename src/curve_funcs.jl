5b51df6f431a01c420dced1937c73f63affd9e60
Derek Tucker <jdtuck@sandia.gov>
2015-09-24 09:53:18 -0600

    finished up first revision of supporting curve functions

--- src/curve_funcs.jl
+++ src/curve_funcs.jl
@@ -80,18 +80,25 @@ function q_to_curve(q)
 end
 
 
-function optimum_reparam(q1::Array{Float64,2}, q2::Array{Float64,2},
+function optimum_reparam(beta1::Array{Float64,2}, beta2::Array{Float64,2},
                          lam::Float64=0.0; method::ASCIIString="DP", w=0.01,
-                         beta1::Array{Float64,2}, beta2::Array{Float64,2},
                          rotated::Bool=true, isclosed::Bool=false)
-    n1, M = size(q2);
-    q1i = reshape(q1, M*n1, 1);
-    q2i = reshape(q2, M*n1, 1);
+    n1, M = size(beta2);
     timet = linspace(0,1,M);
     skipm = 4;
     auto = 2;
+    tau = 0;
     if (method == "DP")
+        # Optimze over SO(n) x Gamma
+        q1 = curve_to_q(beta1);
 
+        # Optimzie over SO(n)
+        beta2, R, tau = find_rotation_seed_coord(beta1, beta2);
+        q2 = curve_to_q(beta2);
+
+        # Optimzie over Gamma
+        q1i = reshape(q1, M*n1, 1);
+        q2i = reshape(q2, M*n1, 1);
         G = zeros(M);
         T = zeros(M);
         sizei = Cdouble[0];
@@ -159,7 +166,7 @@ function optimum_reparam(q1::Array{Float64,2}, q2::Array{Float64,2},
 
     gam = (gam-gam[1]) ./ (gam[end] - gam[1]);
 
-    return gam, R
+    return gam, R, tau
 end
 
 
@@ -336,11 +343,287 @@ function group_action_by_gamma(q, gamma)
 end
 
 
+function group_action_by_gamma_coord(f, gamma)
+    n, T = size(f);
+    fn = zeros(n,T);
+
+    for j = 1:n
+        s = Spline1D(linspace(0,1,T), f[j, :]);
+        fn[j,:] = evaluate(s, gamma);
+    end
+
+    return fn
+end
+
+
+function project_curve(q)
+    T = size(q,2);
+    tol = 1e-5;
+    maxit = 200;
+    itr = 0;
+    delta = 0.5;
+    x = q_to_curve(q);
+    a = -1 * calculatecentroid(x);
+
+    psi1, psi2, psi3, psi4 = psi(x,a,q);
+
+    r = [psi3,psi4];
+    rnorm = zeros(maxit+1);
+    rnomr[1] = norm(r);
+
+    while itr <= maxit
+        basis = find_basis_normal(q);
+
+        # calculate Jacobian
+        J = calc_j(basis);
+
+        # Newton-Raphson step to update q
+        y = J\(-r);
+        dq = delta * (y[1]*basis[1] + y[2]*basis[2]);
+        normdq = sqrt(innerprod_q2(dq,dq));
+        q = cos(normdq) * q + sin(normdq) * dq/normdq;
+        q /= sqrt(innerprod_q2(q,q));
+
+        # update x and a from the new q
+        beta_new = q_to_curve(q);
+        x = copy(beta_new);
+        a = -1 * calculatecentroid(x);
+        beta_new = x + repmat(a,1,T);
+
+        # calculate the new value of psi
+        psi1, psi2, psi3, psi4 = psi(x,a,q);
+        r = [psi3,psi4];
+        rnomr[iter+1] = norm(r);
+
+        if norm(r) < tol
+            break
+        end
+
+        itr += 1;
+
+    end
+
+    rnorm = rnorm[1:itr-1];
+
+    return q
+end
+
+
+function pre_proc_curve(beta, T=100)
+    beta = resamplecurve(beta, T);
+    q = curve_to_q(beta);
+    qnew = project_curve(q);
+    x = q_to_curve(qnew);
+    a = -1 * calculatecentroid(x);
+    betanew = x + repmat(a,1,T);
+    A = eye(2)
+
+    return betanew, qnew, A
+end
+
+
+function inverse_exp_coord(beta1, beta2)
+    T = size(beta1,2);
+    centroid1 = calculatecentroid(beta1);
+    beta1 -= repamat(centroid1,1,T);
+    centroid2 = calculatecentroid(beta2);
+    beta2 -= repmat(centroid2,1,T);
+
+    q1 = curve_to_q(beta1);
+
+    # Iteratively optimize over SO(n) x Gamma using old DP
+    gam, R, tau = optimum_reparam(beta1, beta2);
+    beta2 = R * shift_f(beta2, tau);
+    gamI = invertGamma(gam);
+    beta2 = group_action_by_gamma_coord(beta2, gamI);
+    beta2, R, tau = find_rotation_seed_coord(beta1, beta2);
+    q2n = curve_to_q(beta2);
+
+    # Compute geodesic distance
+    q1dotq2 = innerprod_q2(q1, q2n);
+    dist = acos(q1dotq2);
+
+    # Compute shooting vector
+    if q1dotq2 > 1
+        q1dotq2 = 1.;
+    end
+
+    u = q2n - q1dotq2 * q1;
+    normu = sqrt(innerprod_q2(u, u));
+
+    if normu > 1e-4
+        v = u * acos(q1dotq2)/normu;
+    else
+        v = zeros(2, T);
+    end
+
+    return v, dist
+end
+
+
+function inverse_exp(q1, q2, beta2)
+    T = size(q1,2);
+    centroid1 = calculatecentroid(beta2);
+    beta2 -= repmat(centroid1,1,T);
+
+    # Optimize over SO(n) x Gamma
+    beta1 = q_to_curve(q1);
+    gam, R, tau = optimum_reparam(beta1, beta2);
+    gamI = invertGamma(gam);
+    beta2 = R * shift_f(beta2, tau);
+
+    # Applying optimal re-parameterization to the second curve
+    beta2 = group_action_by_gamma_coord(beta2, gamI);
+    q2 = curve_to_q(beta2);
+
+    # Optimize over SO(n)
+    q2, R2, tau = find_rotation_and_seed_q(q1,q2);
+
+    # Compute geodesic distance
+    q1dotq2 = innerprod_q2(q1, q2);
+    dist = acos(q1dotq2);
+
+    # Compute shooting vector
+    if q1dotq2 > 1
+        q1dotq2 = 1.;
+    end
+
+    u = q2 - q1dotq2 * q1;
+    normu = sqrt(innerprod_q2(u, u));
+
+    if normu > 1e-4
+        v = u * acos(q1dotq2) / normu;
+    else
+        v = zeros(2, T);
+    end
+
+    return v
+end
+
+
+function gram_schmidt(basis)
+    b1 = basis[1];
+    b2 = basis[2];
+
+    basis1 = b1 / sqrt(innerprod_q2(b1, b1));
+    b2 = b2 - innerprod_q2(basis1,b2)*basis1;
+    basis2 = b2 / sqrt(innerprod_q2(b2, b2));
+
+    basis_o = Array(any,2);
+    basis_o[1] = basis1;
+    basis_o[2] = basis2;
+
+    return basis_o
+end
+
+
+function project_tangent(w, q, basis)
+    w = w - innerprod_q2(w, q) * q;
+    bo = gram_schmidt(basis);
+
+    wproj = w - innerprod_q2(w, bo[1])*bo[1] - innerprod_q2(w,bo[1])*bo[1];
+
+    return wproj
+end
+
+
+function scale_curve(beta)
+    n, T = size(beta);
+    normbetadot = zeros(T);
+    betadot = zeros(n,T);
+    for i = 1:n
+        betadot[n,:] = gradient(beta[n,:], 1.0/(T-1));
+    end
+    for i = 1:T
+        normbetadot[i] = norm(betadot[:,i]);
+    end
+    scale = trapz(linspace(0,1,T), normbetadot);
+    beta_scaled = beta / scale;
+
+    return beta_scaled, scale
+end
+
+
+function parallel_translate(w, q1, q2, basis, mode='O')
+    wtilde = w - 2 innerprod_q2(w,q2) / innerprod_q2(q1+q2, q1+q2) * (q1+q2);
+    l = sqrt(innerprod_q2(wtilde, wtilde));
+
+    if mode == 'C'
+        wbar = project_tangent(wtilde, q2, basis);
+        normwbar = sqrt(innerprod_q2(wbar, wbar));
+        if normwbar > 1e-4
+            wbar = wbar * l / normwbar;
+        end
+    else
+        wbar = wtilde;
+    end
+
+    return wbar
+end
+
+
+function curve_zero_crossing(Y, beta, bt, y_max, y_min, gmax, gmin)
+    T = size(beta,2);
+    betanu = q_to_curve(bt);
+    max_itr = 100;
+    a = zeros(max_itr);
+    a[1] = 1.;
+    f = zeros(max_itr);
+    f[1] = y_max - Y;
+    f[2] = y_min - Y;
+    mrp = f[1];
+    mrn = f[2];
+    mrp_ind = 1;  # most recent positive index
+    mrn_ind = 2;  # most recent negative index
+
+    for ii = 3:max_itr
+        x1 = a[mrp_ind];
+        x2 = a[mrn_ind];
+        y1 = mrp;
+        y2 = mrn;
+        a[ii] = (x1 * y1 - x2 * y2) / float(y2-y1);
+
+        beta1, O_hat, tau = find_rotation_seed_coord(betanu, beta);
+        gamma = a[ii] * gmax + (1-a[ii]) * gmin;
+
+        beta1 = group_action_by_gamma_coord(beta1, gamma);
+        beta1, O_hat1, tau = find_rotation_seed_coord(betanu, beta1);
+        q1 = curve_to_q(beta1);
+        f[ii] = innerprod_q2(q1, bt) - Y;
+
+        if abs(f[ii]) < 1e-5
+            break
+        elseif f[ii] > 0
+            mrp = f[ii];
+            mrp_ind = ii;
+        else
+            mrn = f[ii];
+            mrn_ind = ii;
+        end
+    end
+
+   gamma = a[ii] * gmax + (1-a[ii]) * gmin;
+
+   beta1, O_hat, tau = find_rotation_seed_coord(betanu, beta1);
+   beta1 = group_action_by_gamma_coord(beta1, gamma);
+   beta1, O_hat1, tau = find_rotation_seed_coord(betanu, beta1);
+   O_hat = O_hat*O_hat1;
+
+  return gamma, O_hat, tau
+end
+
+
+function rot_mat(theta)
+    O = [cos(theta), -1*sin(theta); sin(theta), cos(theta)];
+
+    return O
+end
+
+
 function innerprod_q2(q1, q2)
     T = size(q1,2);
     val = sum(sum(q1.*q2))/T
 
     return val
 end
-
 
