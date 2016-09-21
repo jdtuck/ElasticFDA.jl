1eb8d6365a71c1dc2f9dab2edc966f13d47657d7
Derek Tucker <jdtuck@sandia.gov>
2016-09-20 21:16:53 -0600

    update appveyor and windows build

--- deps/build.jl
+++ deps/build.jl
@@ -15,9 +15,9 @@ end
     # these binaries were cross-compiled from Cygwin for x86_64 only using
     # the Makefile_win in the corresponding src directories and the windows bin
     # release of openblas
-    run(`curl -LO https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.3.0/gropt.7z`)
-    run(`curl -LO https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.3.0/fdasrsf.7z`)
-    run(`curl -LO https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.3.0/fdaqmap.7z`)
+    BinDeps.download_cmd("https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/fdaqmap.7z")
+    BinDeps.download_cmd("https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/fdasrsf.7z")
+    BinDeps.download_cmd("https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.3.0/fdaqmap.7z")
     run(`7z x -y fdasrsf.7z`)
     run(`7z x -y gropt.7z`)
     run(`7z x -y fdaqmap.7z`)
