fe9852231d04672c0792f2ca7adf754da0874f0a
Derek Tucker <jdtuck@sandia.gov>
2016-09-20 21:43:23 -0600

    fix warnings

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 if is_unix()
     cd(joinpath(dirname(@__FILE__), "src", "fdasrsf"))
-    suffix = @osx? "dylib" : "so"
+    suffix = is_apple() ? "dylib" : "so"
     run(`make clean`)
     run(`make SUFFIX=$suffix`)
     cd(joinpath(dirname(@__FILE__), "src", "gropt"))
@@ -10,13 +10,21 @@ if is_unix()
     run(`make clean`)
     run(`make SUFFIX=$suffix`)
 else # Windows
-    using BinDeps
     # these binaries were cross-compiled from Cygwin for x86_64 only using
     # the Makefile_win in the corresponding src directories and the windows bin
     # release of openblas
-    BinDeps.download_cmd("https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/fdaqmap.7z", "fdaqmap.7z")
-    BinDeps.download_cmd("https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/fdasrsf.7z", "fdasrsf.7z")
-    BinDeps.download_cmd("https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/gropt.7z", "gropt.7z")
+    url1 = "https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/fdaqmap.7z"
+    url2 = "https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/fdasrsf.7z"
+    url3 = "https://github.com/jdtuck/ElasticFDA.jl/releases/download/v0.4.0/gropt.7z"
+    try
+        run(`curl -LO $url1`)
+        run(`curl -LO $url2`)
+        run(`curl -LO $url3`)
+    catch
+        run(`powershell -Command "(new-object net.webclient).DownloadFile(\"$url1\", \"fdaqmap.7z\")"`)
+        run(`powershell -Command "(new-object net.webclient).DownloadFile(\"$url2\", \"fdasrsf.7z\")"`)
+        run(`powershell -Command "(new-object net.webclient).DownloadFile(\"$url3\", \"gropt.7z\")"`)
+    end
     run(`7z x -y fdaqmap.7z`)
     run(`7z x -y fdasrsf.7z`)
     run(`7z x -y gropt.7z`)
